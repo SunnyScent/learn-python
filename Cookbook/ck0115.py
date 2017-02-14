5239cd53f6833859f0690d68c0a66facf7006f55
SunnyScent <C26293@icloud.com>
2017-02-14 22:27:32 +0800

    commit

---
+++ Cookbook/ck0115.py
@@ -0,0 +1,35 @@
+"""Grouping Records Together Based on a Field"""
+
+rows = [
+    {'address': '5412 N CLARK', 'date': '07/01/2012'},
+    {'address': '5148 N CLARK', 'date': '07/04/2012'},
+    {'address': '5800 E 58TH', 'date': '07/02/2012'},
+    {'address': '2122 N CLARK', 'date': '07/03/2012'},
+    {'address': '5645 N RAVENSWOOD', 'date': '07/02/2012'},
+    {'address': '1060 W ADDISON', 'date': '07/02/2012'},
+    {'address': '4801 N BROADWAY', 'date': '07/01/2012'},
+    {'address': '1039 W GRANVILLE', 'date': '07/04/2012'},
+]
+
+from itertools import groupby
+from operator import itemgetter
+
+# Sort by the desired filed first
+rows.sort(key=itemgetter('date'))
+print(rows)
+# Iterate In groups
+for date, items in groupby(rows, key=itemgetter('date')):
+    print(date)
+    for i in items:
+        print('    ', i)
+
+from collections import defaultdict
+
+rows_by_date = defaultdict(list)
+print(rows_by_date)
+for row in rows:
+    rows_by_date[row['date']].append(row)
+
+# Example
+for r in rows_by_date['07/01/2012']:
+    print(r)
