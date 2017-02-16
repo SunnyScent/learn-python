34b73a7b05a4adc829c9422e30bbb890d08469ff
SunnyScent <C26293@icloud.com>
2017-02-16 22:41:12 +0800

    commit

--- Cookbook/ck0202.py
+++ Cookbook/ck0202.py
@@ -1,1 +1,23 @@
 """字符串开头或者结尾匹配"""
+filename = 'spam.txt'
+print(filename.endswith('.txt'))
+print(filename.startswith('file:'))
+url = 'http://www.python.org'
+print(url.startswith('http:'))
+
+import os
+
+filenames = os.listdir('.')
+print(filenames)
+print([name for name in filenames if name.endswith(('.c', '.h'))])
+print(any(name.endswith('.py') for name in filenames))
+
+import urllib.request
+
+
+def read_data(name):
+    if name.endswith(('http', 'https', 'ftp')):
+        return urllib.request.urlopen(name).open
+    else:
+        with open(name) as f:
+            return f.read()
